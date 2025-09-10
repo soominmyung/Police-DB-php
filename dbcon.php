a01bb98dea7fa2d0e65c38e3d6854c24d5868784
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-10 17:24:41 +0100

    Delete dbcon.php

--- dbcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = getenv('DB_HOST');
-$username   = getenv('DB_USER');
-$password   = getenv('DB_PASS');
-$dbname     = getenv('DB_NAME');
-$conn = mysqli_connect($servername, $username, $password, $dbname) or die('Connection failed');
-mysqli_set_charset($conn, 'utf8mb4');
-?>

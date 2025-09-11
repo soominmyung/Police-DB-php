1495b92405cd693f12468e72c4325b8ac891cce1
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-11 02:16:02 +0100

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -5,7 +5,8 @@ $user = getenv('DB_USER') ?: getenv('MYSQLUSER');
 $pass = getenv('DB_PASS') ?: getenv('MYSQLPASSWORD');
 $db   = getenv('DB_NAME') ?: getenv('MYSQL_DATABASE');
 $port = intval(getenv('DB_PORT') ?: getenv('MYSQLPORT') ?: 3306);
-
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 mysqli_set_charset($conn, 'utf8mb4');

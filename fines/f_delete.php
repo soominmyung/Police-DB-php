7a9de793caa8a5d03637c1f596dfe5e99b491847
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-11 04:31:08 +0100

    Update f_delete.php

--- fines/f_delete.php
+++ fines/f_delete.php
@@ -1,7 +1,14 @@
 <?php
+if (getenv('DEMO_MODE') === '1') {
+    http_response_code(200); 
+    echo "Public demo is read only. New, edit and delete are disabled. "
+       . "Please run the project locally to try full functionality.";
+    exit;
+}
 require('../dbcon.php');
 $Fine_ID=$_REQUEST['Fine_ID'];
 $sql = "DELETE FROM Fines WHERE Fine_ID=".$Fine_ID.";"; 
 $result = mysqli_query($conn,$sql) or die ("Delete failed.");
 header("Location: fines.php"); 
 ?>
+

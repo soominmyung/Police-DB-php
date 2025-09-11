f817cb4831864b59c1fb05b957c01d72fe83206a
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-11 01:43:59 +0100

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -3,7 +3,7 @@
 $host = getenv('DB_HOST') ?: getenv('MYSQLHOST') ?: 'mysql.railway.internal';
 $user = getenv('DB_USER') ?: getenv('MYSQLUSER');
 $pass = getenv('DB_PASS') ?: getenv('MYSQLPASSWORD');
-$db   = getenv('DB_NAME') ?: getenv('MYSQLDATABASE');
+$db   = getenv('DB_NAME') ?: getenv('MYSQL_DATABASE');
 $port = intval(getenv('DB_PORT') ?: getenv('MYSQLPORT') ?: 3306);
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);

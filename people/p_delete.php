1b3dea7a3a24b41fa58c7ac062d7410ea22e63b9
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-11 04:32:36 +0100

    Update p_delete.php

--- people/p_delete.php
+++ people/p_delete.php
@@ -1,7 +1,14 @@
 <?php
+if (getenv('DEMO_MODE') === '1') {
+    http_response_code(200); 
+    echo "Public demo is read only. New, edit and delete are disabled. "
+       . "Please run the project locally to try full functionality.";
+    exit;
+}
 require('../dbcon.php');
 $People_ID=$_REQUEST['People_ID'];
 $sql = "DELETE FROM People WHERE People_ID=".$People_ID.";"; 
 $result = mysqli_query($conn,$sql) or die ("Delete failed.");
 header("Location: people.php"); 
+
 ?>

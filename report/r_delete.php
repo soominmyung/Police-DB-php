15dfe1337126a2b6502f2a237f182d6b9d5616a1
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-11 04:32:53 +0100

    Update r_delete.php

--- report/r_delete.php
+++ report/r_delete.php
@@ -1,7 +1,14 @@
 <?php
+if (getenv('DEMO_MODE') === '1') {
+    http_response_code(200); 
+    echo "Public demo is read only. New, edit and delete are disabled. "
+       . "Please run the project locally to try full functionality.";
+    exit;
+}
 require('../dbcon.php');
 $Incident_ID=$_REQUEST['Incident_ID'];
 $sql = "DELETE FROM Incident WHERE Incident_ID=".$Incident_ID.";"; 
 $result = mysqli_query($conn,$sql) or die ("Delete failed.");
 header("Location: report.php"); 
 ?>
+

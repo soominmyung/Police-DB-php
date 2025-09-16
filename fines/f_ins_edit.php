f7562beafa2c7fbc19501f22b9967bcb948c9588
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-16 16:40:34 +0100

    Update f_ins_edit.php

--- fines/f_ins_edit.php
+++ fines/f_ins_edit.php
@@ -2,12 +2,12 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-if (getenv('DEMO_MODE') === '1') {
-    http_response_code(200); 
-    echo "Public demo is read only. New, edit and delete are disabled. "
-       . "Please run the project locally to try full functionality.";
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && getenv('DEMO_MODE') === '1') {
+    echo "<script>alert('Public demo is read only. New, edit and delete are disabled.');</script>";
+    echo "<script>window.location.replace('fines.php')</script>";
     exit;
 }
+
 // Include config file
 require_once "../dbcon.php";
 
@@ -383,4 +383,5 @@ if (isset($_POST['new']) && $_POST['new'] == 1) {
 
 
 </html>
+
 

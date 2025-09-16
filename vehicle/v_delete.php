49fa3f981ecb44975d2ecaf69e60fcc6c0135120
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-16 16:56:07 +0100

    Update v_delete.php

--- vehicle/v_delete.php
+++ vehicle/v_delete.php
@@ -3,6 +3,7 @@ if (getenv('DEMO_MODE') === '1') {
     http_response_code(200); 
     echo "Public demo is read only. New, edit and delete are disabled. "
        . "Please run the project locally to try full functionality.";
+    echo "<script>window.location.replace('vehicle.php')</script>";
     exit;
 }
 require('../dbcon.php');
@@ -11,4 +12,5 @@ $sql = "DELETE FROM Vehicle WHERE Vehicle_ID=".$Vehicle_ID.";";
 $result = mysqli_query($conn,$sql) or die ("Delete failed.");
 header("Location: vehicle.php"); 
 ?>
+
 

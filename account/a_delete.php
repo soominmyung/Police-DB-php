94e55af18404de9d3645ff23022abe22fd0c5291
Soomin Myung <121304476+soominmyung@users.noreply.github.com>
2025-09-16 17:05:20 +0100

    Update a_delete.php

--- account/a_delete.php
+++ account/a_delete.php
@@ -1,9 +1,10 @@
 <?php
 if (getenv('DEMO_MODE') === '1') {
     http_response_code(200); 
-    echo "Public demo is read only. New, edit and delete are disabled. "
-       . "Please run the project locally to try full functionality.";
-    echo "<script>window.location.replace('account.php')</script>";
+    echo "<script>
+        alert('Public demo is read only. New, edit and delete are disabled.\\nPlease run the project locally to try full functionality.');
+        window.location.replace('account.php');
+    </script>";
     exit;
 }
 require('../dbcon.php');
@@ -12,5 +13,6 @@ $sql = "DELETE FROM Users WHERE Users_ID=".$Users_ID.";";
 $result = mysqli_query($conn,$sql) or die ("Delete failed.");
 header("Location: account.php"); 
 ?>
+
 
 
